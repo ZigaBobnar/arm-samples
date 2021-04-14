0b0a66eca13850454555fd13ca3fe617239c7a46
Ziga Bobnar <ziga@bobnar.net>
2021-04-14 12:52:14 +0200

    Add cyclic buffer project

---
+++ 07-cyclic_buffer/cyclic_buffer/app/src/main.c
@@ -0,0 +1,35 @@
+#include <asf.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+int main (void)
+{
+
+    /* sets the processor clock according to conf_clock.h definitions */
+    sysclk_init();
+
+    /* disable wathcdog */
+    WDT->WDT_MR = WDT_MR_WDDIS;
+
+    /********************* HW init     ***************************/
+
+
+
+
+
+    /********************* Main loop     ***************************/
+    while(1)
+    {
+    }
+
+
+    while(1) {}
+}
+
+
+#ifdef __cplusplus
+}
+#endif
