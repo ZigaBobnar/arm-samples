a4129ece69dbd2bc85a7eb0123ac391d8ad669e9
Ziga Bobnar <ziga@bobnar.net>
2021-04-30 11:23:03 +0200

    Finish the cyclic buffer program

--- 07-cyclic_buffer/cyclic_buffer/app/src/rtos_tasks.c
+++ 07-cyclic_buffer/cyclic_buffer/app/src/rtos_tasks.c
@@ -22,10 +22,6 @@ fifo_t button_presses_fifo = { 0, 0, KEYS_FIFO_SIZE - 1, button_presses_buffer }
 
 uint8_t old_buttons_state = 0x00;
 
-char queue[10] = "          ";
-
-uint32_t last_timer_value = 0;
-
 void rtos_tasks_init(void) {
     // Init buttons and LED
     ioport_init();
@@ -45,7 +41,7 @@ void rtos_tasks_init(void) {
 
     // Init lcd
     lcd_init(&lcd);
-    sprintf(lcd._lcd_string, "Inicializacija...");
+    sprintf(lcd._lcd_string, "Inicializacija..");
     lcd_write_lcd_string(&lcd);
 
     // Init timer counter
@@ -53,7 +49,11 @@ void rtos_tasks_init(void) {
 
     tc_init(TC0, 1, TC_CMR_TCCLKS_TIMER_CLOCK3 | TC_CMR_WAVE);
     tc_start(TC0, 1);
-    last_timer_value = tc_read_cv(TC0, 1);
+
+    delay_ms(500);
+
+    sprintf(lcd._lcd_string, "                                ");
+    lcd_write_lcd_string(&lcd);
 }
 
 
@@ -66,18 +66,22 @@ fifo_t keys_fifo = { 0, 0, KEYS_FIFO_SIZE, keys_buff };
 
 rtos_task_t buttons_task = {
     .function = buttons_task_runtime,
+    .task_name = "Buttons",
 };
 
 rtos_task_t led_driver_task = {
     .function = led_driver_task_runtime,
+    .task_name = "LED_Driver",
 };
 
 rtos_task_t lcd_driver_task = {
     .function = lcd_driver_task_runtime,
+    .task_name = "LCD_Driver",
 };
 
 rtos_task_t null_task = {
     .function = NULL,
+    .task_name = "<end>",
 };
 
 rtos_task_t* rtos_tasklist[] = {
@@ -94,7 +98,7 @@ void buttons_task_runtime(void) {
             (!ioport_get_pin_level(PIO_PC24_IDX) << 1) |
             (!ioport_get_pin_level(PIO_PC23_IDX) << 0);
 
-    uint8_t rising_edge = ~old_buttons_state & new_buttons_state;
+    // uint8_t rising_edge = ~old_buttons_state & new_buttons_state;
     uint8_t falling_edge = old_buttons_state & ~new_buttons_state;
 
     if ((falling_edge >> 3) & 1) {
@@ -114,21 +118,75 @@ void buttons_task_runtime(void) {
 }
 
 void led_driver_task_runtime(void) {
-    //delay_us(100);
+    static enum { READY, PULSE_HIGH, PULSE_LOW, PAUSE } state = READY;
+    static uint32_t state_start_timer_value = 0;
+    static uint32_t remaining_pulses = 0;
+
+    if (state == PULSE_HIGH) {
+        if (tc_read_cv(TC0, 1) - state_start_timer_value >= 40 * (CLOCKS_PER_SEC * 32)) {
+            state = PULSE_LOW;
+
+            ioport_set_pin_level(PIO_PC22_IDX, 0);
+
+            state_start_timer_value = tc_read_cv(TC0, 1);
+        }
+    } else if (state == PULSE_LOW) {
+        if (tc_read_cv(TC0, 1) - state_start_timer_value >= 60 * (CLOCKS_PER_SEC * 32)) {
+            if (remaining_pulses > 0) {
+                state = PULSE_HIGH;
+                remaining_pulses--;
+
+                ioport_set_pin_level(PIO_PC22_IDX, 1);
+            } else {
+                state = PAUSE;
+            }
+
+            state_start_timer_value = tc_read_cv(TC0, 1);
+        }
+    } else if (state == PAUSE) {
+        if (tc_read_cv(TC0, 1) - state_start_timer_value >= 1000 * (CLOCKS_PER_SEC * 32)) {
+            state = READY;
+        }
+    } else {
+        uint8_t value = 0;
+        uint8_t read = fifo_read(&button_presses_fifo, &value, 1);
+
+        if (read == 1) {
+            state = PULSE_HIGH;
+            remaining_pulses =
+                value == '1' ? 0 :
+                (value == '2' ? 1 :
+                (value == '3' ? 2 :
+                (value == '4' ? 3 :
+                0)));
+
+            ioport_set_pin_level(PIO_PC22_IDX, 1);
+
+            state_start_timer_value = tc_read_cv(TC0, 1);
+        }
+    }
 }
 
 void lcd_driver_task_runtime(void) {
-    uint8_t pending_buttons[KEYS_FIFO_SIZE];
-    uint8_t actual_pending = 0;
+    static uint32_t last_display_update_timer_value = 0;
 
-    actual_pending = fifo_peek(&button_presses_fifo, pending_buttons, KEYS_FIFO_SIZE - 1);
+    if (tc_read_cv(TC0, 1) - last_display_update_timer_value >= 100 * (CLOCKS_PER_SEC * 32)) {
+        char queue[10] = "         ";
 
-    for (int i = 0; i < 16; ++i) {
-        queue[i] = (i < actual_pending) ? pending_buttons[i] : ' ';
+        uint8_t pending_buttons[KEYS_FIFO_SIZE];
+        uint8_t actual_pending = 0;
+
+        actual_pending = fifo_peek(&button_presses_fifo, pending_buttons, KEYS_FIFO_SIZE - 1);
+
+        for (int i = 0; i < 10; ++i) {
+            queue[i] = (i < actual_pending) ? pending_buttons[i] : ' ';
+        }
+
+        sprintf(lcd._lcd_string, queue);
+        lcd_write_lcd_string(&lcd);
+
+        last_display_update_timer_value = tc_read_cv(TC0, 1);
     }
-
-    sprintf(lcd._lcd_string, queue);
-    lcd_write_lcd_string(&lcd);
 }
 
 #ifdef __cplusplus
